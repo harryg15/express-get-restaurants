1e17f2efa3a72a71138c0ccde2cfddbf7169c23d
harryg15 <harry.grant99@googlemail.com>
2023-02-22 10:52:59 +0000

    added CRUD functionality to express sqlite databases

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
 
+app.use(express.json())
+
 app.listen(port, () => {
     sequelize.sync();
     console.log("Your server is listening on port " + port);
@@ -20,4 +22,22 @@ app.get('/restaurants', async (req, res) => {
 app.get('/restaurants/:id', async (req, res) => {
     const getSpecific = await Restaurant.findByPk(req.params.id)
     res.json(getSpecific)
+})
+
+//create
+app.post('/restaurants', async (req, res) => {
+    await Restaurant.create(req.body)
+    res.send(await Restaurant.findAll())
+})
+
+//update
+app.put('/restaurants/:id', async (req, res) => {
+    await Restaurant.update(req.body, {where: {id: req.params.id}})
+    res.send(await Restaurant.findAll())
+})
+
+//delete
+app.delete('/restaurants/:id', async (req, res) => {
+    await Restaurant.destroy({where: {id: req.params.id}})
+    res.send(await Restaurant.findAll())
 })
