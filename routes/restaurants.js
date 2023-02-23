0bbd083a08f70ec5ef377c0925a4c16f68972c01
harryg15 <harry.grant99@googlemail.com>
2023-02-23 11:33:36 +0000

    added express router to restaurant database & included bonus section where we specify the number of characters

---
+++ routes/restaurants.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const router = express.Router()
+const {Restaurant} = require("../models/index")
+const {check, validationResult, isLength} = require('express-validator')
+
+router.use(express.json())
+
+//get all
+router.get('/', async (req, res) => {
+    const getRestaurants = await Restaurant.findAll()
+    res.json(getRestaurants)
+})
+
+//get specific
+router.get('/:id', async (req, res) => {
+    const getSpecific = await Restaurant.findByPk(req.params.id)
+    res.json(getSpecific)
+})
+
+//create
+router.post('/', [
+    check("name").not().isEmpty().trim().isLength({min: 10, max: 30}),
+    check("location").not().isEmpty().trim().isLength({min: 10, max: 30}),
+    check("cuisine").not().isEmpty().trim().isLength({min: 10, max: 30}),
+
+], async (req, res) => {
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+        res.send({error: errors.array()})
+    } else {
+        await Restaurant.create(req.body)
+        res.send(await Restaurant.findAll())
+    }
+})
+
+//update
+router.put('/:id', async (req, res) => {
+    await Restaurant.update(req.body, {where: {id: req.params.id}})
+    res.send(await Restaurant.findAll())
+})
+
+//delete
+router.delete('/:id', async (req, res) => {
+    await Restaurant.destroy({where: {id: req.params.id}})
+    res.send(await Restaurant.findAll())
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,45 +1,12 @@
 const express = require("express");
 const app = express();
-const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
-
 const port = 3000;
+const restaurantRoutes = require("./routes/restaurants")
 
-//TODO: Create your GET Request Route Below: 
-
-app.use(express.json())
+app.use("/restaurants", restaurantRoutes)
 
 app.listen(port, () => {
     sequelize.sync();
     console.log("Your server is listening on port " + port);
-})
-
-//get all
-app.get('/restaurants', async (req, res) => {
-    const getRestaurants = await Restaurant.findAll()
-    res.json(getRestaurants)
-})
-
-//get specific
-app.get('/restaurants/:id', async (req, res) => {
-    const getSpecific = await Restaurant.findByPk(req.params.id)
-    res.json(getSpecific)
-})
-
-//create
-app.post('/restaurants', async (req, res) => {
-    await Restaurant.create(req.body)
-    res.send(await Restaurant.findAll())
-})
-
-//update
-app.put('/restaurants/:id', async (req, res) => {
-    await Restaurant.update(req.body, {where: {id: req.params.id}})
-    res.send(await Restaurant.findAll())
-})
-
-//delete
-app.delete('/restaurants/:id', async (req, res) => {
-    await Restaurant.destroy({where: {id: req.params.id}})
-    res.send(await Restaurant.findAll())
 })
